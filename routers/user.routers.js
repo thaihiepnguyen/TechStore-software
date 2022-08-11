3929b3729b031e3383430156250eea7f4c29630d
thaihiep232002 <thaihiep232002@gmail.com>
2022-08-11 21:33:33 +0700

    updateeee

--- routers/user.routers.js
+++ routers/user.routers.js
@@ -6,42 +6,13 @@ const controller = require('../controllers/user.controllers');
 const app = express();
 app.use(bodyParser.json())
 
-// app.get('/login', (req, res) => {
-//   res.render('user/login')
-// })
-
-// app.get('/signup', (req, res) => {
-//   res.render('user/signup')
-// })
-
-// app.post('/api/register', async (req,res) => {
-//     const user = new userModel(req.body)
-
-//     try {
-//         await user.save()
-//         res.send(user)
-//     }catch(error) {
-//         res.status(500).send(error)
-//     }
-// })
-
-// app.get("/users", async (request, response) => {
-//   const users = await userModel.find({});
-
-//   try {
-//     response.send(users);
-//   } catch (error) {
-//     response.status(500).send(error);
-//   }
-// });
-
 router.get('/login', controller.getLoginPage);
 
-router.post('/login', controller.handleLogin);
+router.post('/api/login', controller.handleLogin);
 
 router.get('/signup', controller.getSignupPage);
 
-router.post('/signup', controller.handleSignup);
+router.post('/api/signup', controller.handleSignup);
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -1,49 +1,3 @@
-// const {MongoClient} = require('mongodb');
-// const uri = 'mongodb+srv://doananhduong:duong1234@cluster0.lgai80j.mongodb.net/?retryWrites=true&w=majority';
-// const client = new MongoClient(uri);
-// client.connect();
-
-// async function listDatabases(client){
-//     databasesList = await client.db().admin().listDatabases();
- 
-//     console.log("Databases:");
-//     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-// };
-
-// async function createCustomer(client, newCustomer) {
-//     const res = await client.db('TechStore').collection('Account').insertOne(newCustomer);
-//     console.log(`New customer created with the following id: ${res.insertedId}`);
-// }
-// async function findCustomer(client, name) {
-//     const res = await client.db('TechStore').collection('Account').find(name);
-//     if(err) {
-//         console.log('Cannot find any account');
-//     }
-//     else {
-//         console.log(`Find account with id: ${res}`);
-//     }
-//     return res;
-// }
-//         // await createCustomer(client, {
-//         //     username: "doananhduong",
-//         //     password: "duong1234",
-//         //     gmail: "doananhduong09.10@gmail.com",
-//         //     address: Object({
-//         //         st: "Huynh Tan Phat",
-//         //         city :"Ho Chi Minh",
-//         //         country: "Viet Nam"
-//         //     }),
-//         //     phone: "0973822287",
-//         //     gender: "M",
-//         //     dob: new Date("01/01/2000"), //error
-//         //     creditCard: Object({
-//         //         cardId: 9704229211508648,
-//         //         name_on_card: "DOAN ANH DUONG",
-//         //         expire: new Date("01/2024"), //error
-//         //         cvv: 241
-//         //     })
-//         // });
-        
 const express = require("express")
 const path = require("path")
 const bodyParser = require("body-parser")
@@ -51,7 +5,6 @@ const mongoose = require("mongoose")
 const userRouter = require("./routers/user.routers.js")
 const Users = require('./model/user.models')
 
-// or mongodb://localhost:27017/<database-name>
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 
 const db = mongoose.connection;
@@ -91,16 +44,6 @@ app.get('/cart', (req, res) => {
 app.get('/checkout', (req, res) => {
   res.render('checkout.pug')
 })
-
-// app.get('/user/login', (req, res) => {
-//   res.render('user/login.pug')
-// })
-
-// app.post('/user/login', async (req, res) => {
-//   console.log(req.body);
-//   await Users.create(req.body);
-// })
-
 
 app.use('/user', userRouter);
 const PORT = 2000;
